eb53d3823fecffa32f4a0f3751e8ddb188610f5d
Yehor <74692289+YehorSkudilov@users.noreply.github.com>
2025-04-30 14:07:51 -0700

    refactor: reorganize project structure and update routing for frontend and backend

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-import healthConnect from './routes/healthConnect.js';
+import healthConnect from './backend/routes/healthConnect.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,16 +12,24 @@ const port = process.env.PORT || 8100;
 
 app.use(express.json());
 
+//Frontend
+app.use("/js", express.static("./frontend/js"));
+app.use("/css", express.static("./frontend/css"));
+app.use("/images", express.static("./frontend/assets/images"));
+app.use("/videos", express.static("./frontend/assets/videos"));
+app.use("/fonts", express.static("./frontend/assets/fonts"));
+app.use("/views", express.static("./frontend/views"));
+
+//Backend
+app.use("/config", express.static("./backend/config"));
 app.use('/api/healthConnect', healthConnect);
 
 
-app.use(express.static('public'))
-
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/html/main.html')
+  res.sendFile(path.join(__dirname, 'frontend/views/main.html'))
 })
 app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/public/html/about.html')
+res.sendFile(path.join(__dirname, 'frontend/views/about.html'))
 })
 
 app.listen(port, () => {
