fe0f1a802b25ab92b8e6492750f0c31d2d1a59fb
DMartinez-Van <david.antonio.martinez@gmail.com>
2025-05-02 16:55:25 -0700

    WIP add delete functionality

--- frontend/js/diary.js
+++ frontend/js/diary.js
@@ -1,3 +1,4 @@
+// Add food item to the list on "Add" button click
 document.getElementById("addFoodItem").addEventListener("click", (e) => {
   e.preventDefault();
   // Get form values
@@ -11,11 +12,22 @@ document.getElementById("addFoodItem").addEventListener("click", (e) => {
 
   // Create spans for food and calories
   const details1 = document.createElement("span");
-  const details2 = document.createElement("span");
   details1.classList.add("px-3");
-  details2.classList.add("px-3", "foodItemCalorie");
   details1.textContent = `${quantity}x ${food}`;
-  details2.textContent = `${calories}`;
+
+  const details2 = document.createElement("div");
+  const calroiesValue = document.createElement("span");
+
+  // create img element with src=/images/trash.png
+  const deleteDivContainer = document.createElement("div");
+  deleteDivContainer.classList.add("deleteItem", "px-3");
+  const deleteIcon = document.createElement("img");
+  deleteIcon.src = "/images/trash.png";
+  deleteIcon.alt = "delete";
+
+  details2.classList.add("d-flex", "h-100", "align-items-center");
+  calroiesValue.classList.add("px-3", "foodItemCalorie");
+  calroiesValue.textContent = `${calories}`;
 
   // Append the spans to list item and
   // and append list item to food list
