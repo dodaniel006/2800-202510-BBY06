241d3092c9c9e27cd6b5a965976f862b7ec008bb
DMartinez-Van <david.antonio.martinez@gmail.com>
2025-05-02 12:01:20 -0700

    add food item to list functionality in place

---
+++ frontend/js/diary.js
@@ -0,0 +1,40 @@
+document.getElementById("addFoodItem").addEventListener("click", (e) => {
+  e.preventDefault();
+  // Get form values
+  const food = document.getElementById("foodInput").value;
+  const calories = document.getElementById("calorieInput").value;
+  const quantity = document.getElementById("quantityInput").value;
+
+  // Create a new list item
+  const listItem = document.createElement("li");
+  listItem.classList.add("foodItem", "py-3");
+
+  // Create spans for food and calories
+  const details1 = document.createElement("span");
+  const details2 = document.createElement("span");
+  details1.classList.add("px-3");
+  details2.classList.add("px-3", "foodItemCalorie");
+  details1.textContent = `${quantity}x ${food}`;
+  details2.textContent = `${calories}`;
+
+  // Append the spans to list item and
+  // and append list item to food list
+  listItem.appendChild(details1);
+  listItem.appendChild(details2);
+  document.getElementById("foodList").appendChild(listItem);
+
+  // Clear the input fields
+  document.getElementById("foodInput").value = "";
+  document.getElementById("calorieInput").value = "";
+  document.getElementById("quantityInput").value = "";
+
+  // Send data to server
+  //  fetch("/api/food", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({ food, calories, quantity }),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => console.log("Success:", data))
+  //     .catch((error) => console.error("Error:", error));
+});
