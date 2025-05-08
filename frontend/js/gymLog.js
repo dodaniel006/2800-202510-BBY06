bb171d3d84b4ee60bc49ae4002bf3093bcf1a54a
Daniel Do <dodaniel006@gmail.com>
2025-05-08 12:11:40 -0700

    Add button to gymLog that updates the map target to the user's current location

--- frontend/js/gymLog.js
+++ frontend/js/gymLog.js
@@ -1,17 +1,37 @@
 console.log("Gym Log JS loaded");
 
+const geolocation = new ol.Geolocation({
+  tracking: true,
+  projection: "EPSG:3857",
+});
+
 const map = new ol.Map({
   layers: [
     new ol.layer.Tile({
       source: new ol.source.TileJSON({
-        url: "https://api.maptiler.com/maps/basic-v2/tiles.json?key=jMM6JmEYJJVW4Ydny1Mv",
+        url: "https://api.maptiler.com/maps/streets-v2/tiles.json?key=CtM10uhhBnnLZPJPfCxl",
         tileSize: 512,
       }),
     }),
   ],
   target: "map",
   view: new ol.View({
-    center: ol.proj.fromLonLat([0, 0]),
-    zoom: 2,
+    center: ol.proj.fromLonLat([0, 0]), // Default center
+    zoom: 12,
   }),
+});
+
+document.getElementById("updateLocation").addEventListener("click", () => {
+  geolocation.setTracking(true);
+  console.log("Tracking started");
+});
+
+geolocation.on("change:position", () => {
+  const coordinates = geolocation.getPosition();
+  if (coordinates) {
+    const [lon, lat] = ol.proj.toLonLat(coordinates);
+    console.log("Geolocation position changed:", lat, lon);
+    map.getView().setCenter(ol.proj.fromLonLat([lon, lat]));
+    geolocation.setTracking(false); // Stop tracking after first position
+  }
 });
