f03ea40107e627625f80b03a61518491aa7482ff
Hazel <fulljameshazel@gmail.com>
2025-05-20 17:22:54 -0700

    Update authentication.js

--- web_app/backend/routes/authentication.js
+++ web_app/backend/routes/authentication.js
@@ -91,7 +91,7 @@ authRouter.post("/register", async (req, res) => {
       password, // Password will be hashed by the pre-save hook in User.js schema
       taskList: [],
       roadScore: 0, 
-      ws: {},
+      ws: 0,
     });
 
     await newUser.save();
