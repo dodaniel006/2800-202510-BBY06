00e30738f274aa78dc5286cf1b14291ede46fe30
Jacob Lebl <jlebl@my.bcit.ca>
2025-05-21 20:09:35 -0700

    feat: added brand mail template to sendEmail()
    -New mail.ejs under notifications/views

--- web_app/backend/notifications/mail.js
+++ web_app/backend/notifications/mail.js
@@ -1,6 +1,11 @@
 import FormData from "form-data";
 import Mailgun from "mailgun.js";
+import fs from 'node:fs';
+import path from "path";
+import ejs from "ejs"; 
 import "dotenv/config";
+
+const __dirname = import.meta.dirname;
 
 const mailgun = new Mailgun(FormData);
 const mg = mailgun.client({
@@ -8,32 +13,44 @@ const mg = mailgun.client({
   key: process.env.MAILGUN_API_KEY || "",
 });
 
-// Example
-async function sendSimpleMessage() {
-  try {
-    const data = await mg.messages.create("sandboxac2c816c700d453b99ac833e8d0fe06a.mailgun.org", {
-      from: "Mailgun Sandbox <postmaster@sandboxac2c816c700d453b99ac833e8d0fe06a.mailgun.org>",
-      to: ["Jacob Lebl <lebl.jacob@gmail.com>"],
-      subject: "Hello Jacob Lebl",
-      text: "Hello! This is a test email sent from Mailgun.",
-    });
 
-    console.log(data); // logs response data
-  } catch (error) {
-    console.log(error); //logs any error
-  }
-}
-// End Example
-
-sendSimpleMessage();
-
-
-async function sendEmail(email, subject, text, html) {
-  const mailgun = new Mailgun(FormData);
-  const mg = mailgun.client({
-    username: "api",
-    key: process.env.MAILGUN_API_KEY || "",
+async function sendEmail(recipientEmail, recipientName, subject, contents) {
+  const templatePath = path.join(__dirname, 'views', 'mail.ejs');
+  const templateFile = await fs.promises.readFile(templatePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.errror(err);
+      return;
+    }
+    console.log(data);
   });
+
+  const templateData = {
+    recipientName: recipientName,
+    subject: subject,
+    contents: contents,
+  };
+
+  // Render the EJS template with the data
+  const html = ejs.render(templateFile, templateData);
+
+  try {
+    const emailResult = await mg.messages.create("sandboxac2c816c700d453b99ac833e8d0fe06a.mailgun.org", {
+      from: "Mailgun Sandbox <postmaster@sandboxac2c816c700d453b99ac833e8d0fe06a.mailgun.org>",
+      to: [recipientEmail],
+      subject: subject,
+      html: html
+    });
+    console.log(emailResult);
+  } catch (error) {
+    console.log(error);
+
+  }
+
+
+}export default sendEmail;
+
+
+async function sendBareEmail(email, subject, text, html) {
 
    try {
     const data = await mg.messages.create("sandboxac2c816c700d453b99ac833e8d0fe06a.mailgun.org", {
@@ -50,4 +67,5 @@ async function sendEmail(email, subject, text, html) {
   }
 }
 
-export default sendEmail;
+export { sendBareEmail };
+
